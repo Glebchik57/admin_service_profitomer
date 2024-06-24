a401ae24015c246c0d05256c3554ea81d8db723a
Glebchik57 <g.sevostyanov@inbox.ru>
2024-06-24 21:56:26 +0200

    admin autorization was created

--- admin/admin.py
+++ admin/admin.py
@@ -1,5 +1,8 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
+from flask import session as flask_session, redirect, url_for
+from flask_login import current_user
+
 
 from db_config import session
 from models import (
@@ -15,14 +18,27 @@ from models import (
 )
 
 
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and flask_session.get('is_admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('admin_autorization'))
+
+
 def create_admin(app):
-    admin = Admin(app)
+    admin = Admin(
+        app,
+        index_view=MyAdminIndexView(),
+        name='profit_admin',
+        template_mode='bootstrap4'
+    )
     admin.add_view(ModelView(Users, session))
     admin.add_view(ModelView(Sessions, session))
-    admin.add_view(ModelView(Wb_Orders, session))
-    admin.add_view(ModelView(Wb_Sales, session))
-    admin.add_view(ModelView(Wb_Report_Detail, session))
-    admin.add_view(ModelView(Wb_Stosks, session))
-    admin.add_view(ModelView(Wb_Supplies, session))
-    admin.add_view(ModelView(Delivery, session))
+    admin.add_view(ModelView(Wb_Orders, session, category="Бизнес"))
+    admin.add_view(ModelView(Wb_Sales, session, category="Бизнес"))
+    admin.add_view(ModelView(Wb_Report_Detail, session, category="Бизнес"))
+    admin.add_view(ModelView(Wb_Stosks, session, category="Бизнес"))
+    admin.add_view(ModelView(Wb_Supplies, session, category="Бизнес"))
+    admin.add_view(ModelView(Delivery, session, category="Бизнес"))
     return admin
