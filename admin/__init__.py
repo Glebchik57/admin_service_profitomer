08a20b01cc3d625fb1950f98d73f8bca2189385a
Glebchik57 <g.sevostyanov@inbox.ru>
2024-06-28 23:39:47 +0200

    feat: list of admins was added

--- admin/__init__.py
+++ admin/__init__.py
@@ -11,6 +11,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
 
+ADMINS = ['g.sevostyanov@inbox.ru']
+
 
 def filter_valid_latin1_characters(key):
     return ''.join(char for char in key if char.isascii() and char.isprintable())
@@ -21,6 +23,7 @@ S_KEY = filter_valid_latin1_characters(SECRET_KEY)
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY contains no valid latin-1 characters")
 app.config['SECRET_KEY'] = S_KEY
+app.config['ADMINS'] = ADMINS
 
 login_manager = LoginManager(app)
 
